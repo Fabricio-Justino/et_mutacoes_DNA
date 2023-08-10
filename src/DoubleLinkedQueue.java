bdc406a6a818938a21d547b8c2a57b774cb00987
Fabricio Borges Justino <fabriciobj667@gmail.com>
2023-08-09 23:51:14 -0300

    Terminando classe FIla juntamente com sua classe test

---
+++ src/DoubleLinkedQueue.java
@@ -0,0 +1,166 @@
+import java.util.NoSuchElementException;
+
+/**
+ *
+ * @param <T> : tipo da fila
+ */
+public class DoubleLinkedQueue<T> {
+
+    private class Node {
+        Node next;
+        Node prev;
+
+        T value;
+
+        Node(T value, Node next, Node prev) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+
+        Node(T value) {
+            this(value, null, null);
+        }
+    }
+
+    private int size;
+    private Node header;
+    private Node trailer;
+
+    DoubleLinkedQueue() {
+        this.size = 0;
+        this.header = null;
+        this.trailer = null;
+    }
+
+    /**
+     * Adiciona um elemento no final da fila.
+     * <br>
+     * complexidade: O(1)
+     *
+     * @param value o valor que será inserido
+     */
+    public void addLast(T value) {
+        final Node node = new Node(value, null, this.header);
+
+        if (this.isEmpty())
+            this.trailer = node;
+        else
+            this.header.next = node;
+
+        this.header = node;
+
+        size++;
+    }
+
+    /**
+     * Adiciona um elemento no começo da fila.
+     * <br>
+     * complexidade: O(1)
+     * 
+     * @param value o valor que será inserido
+     */
+    public void addFirst(T value) {
+        final Node node = new Node(value, this.trailer, null);
+
+        if (this.isEmpty())
+            this.header = node;
+        else
+            this.trailer.prev = node;
+
+        this.trailer = node;
+
+        size++;
+
+
+    }
+
+    /**
+     * método que <strong>retorna e remove</strong> o elemento contido no {@link #header}, ou seja, o último elemento da fila.
+     *<br>
+     * complexidade: O(1)
+     * 
+     * @return T: tipo de dado da fila
+     * @throws NoSuchElementException se o <strong>tamanho da fila</strong> for <strong>0</strong>
+     */
+    public T pull() {
+        if (this.isEmpty())
+            throw new NoSuchElementException("The queue is empty", new Throwable("trying to remove non-element"));
+
+        final Node node = this.header;
+        this.header = node.prev;
+
+        if (this.header != null)
+            this.header.next = null;
+
+        this.size--;
+
+        return node.value;
+    }
+
+    /**
+     * método que <strong>retorna e remove</strong> o elemento contido no {@link #trailer}, ou seja, o primeiro elemento da fila.
+     *<br>
+     * complexidade: O(1)
+     * 
+     * @return T: tipo de dado da fila
+     * @throws NoSuchElementException se o <strong>tamanho da fila</strong> for <strong>0</strong>
+     */
+    public T shift() {
+        if (this.isEmpty())
+            throw new NoSuchElementException("The queue is empty", new Throwable("trying to remove non-element"));
+
+        final Node node = this.trailer;
+        this.trailer = node.next;
+
+        if (this.trailer != null)
+            this.trailer.prev = null;
+
+        this.size--;
+        return node.value;
+    }
+
+    /**
+     * método que <strong>somente retorna</strong> o elemento contido no {@link #header}, ou seja, o último elemento da fila.
+     *<br>
+     * complexidade: O(1)
+     *
+     * @return T: tipo de dado da fila ou null se a fila estiver vazia
+     * @throws NoSuchElementException se o <strong>tamanho da fila</strong> for <strong>0</strong>
+     */
+    public T showLast() {
+        return !this.isEmpty() ? this.header.value : null;
+    }
+
+    /**
+     * método que <strong>somente retorna</strong> o elemento contido no {@link #trailer}, ou seja, o primeiro elemento da fila.
+     *<br>
+     * complexidade: O(1)
+     *
+     * @return T: tipo de dado da fila ou null se a fila estiver vazia
+     * @throws NoSuchElementException se o <strong>tamanho da fila</strong> for <strong>0</strong>
+     */
+    public T showFirst() {
+        return !this.isEmpty() ? this.trailer.value : null;
+    }
+    
+    public int size() {
+        return this.size;
+    }
+    
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+
+        builder.append("{");
+        for (Node node = this.trailer; node != null; node = node.next)
+            builder.append(String.format("%s%s", node.value.toString(), (node.next != null ? ", " : "")));
+        builder.append("}");
+
+        return builder.toString();
+    }
+}
---
+++ test/DoubleLinkedQueueTest.java
@@ -0,0 +1,97 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoubleLinkedQueueTest {
+    private DoubleLinkedQueue<Character> queue;
+
+    @BeforeEach
+    void setUp() {
+        this.queue = new DoubleLinkedQueue<>();
+    }
+
+    @Test
+    void addLast() {
+        queue.addLast('a');
+        assertEquals(1, queue.size());
+        assertEquals('a', queue.showLast());
+
+        queue.addLast('b');
+        assertEquals(2, queue.size());
+        assertEquals('b', queue.showLast());
+
+        queue.addLast('c');
+        assertEquals(3, queue.size());
+        assertEquals('c', queue.showLast());
+
+        queue.addFirst('j');
+        assertEquals(4, queue.size());
+        assertEquals('c', queue.showLast());
+    }
+
+    @Test
+    void addFirst() {
+        queue.addFirst('a');
+        assertEquals(1, queue.size());
+        assertEquals('a', queue.showFirst());
+
+        queue.addFirst('b');
+        assertEquals(2, queue.size());
+        assertEquals('b', queue.showFirst());
+
+        queue.addFirst('c');
+        assertEquals(3, queue.size());
+        assertEquals('c', queue.showFirst());
+
+        queue.addLast('j');
+        assertEquals(4, queue.size());
+        assertEquals('c', queue.showFirst());
+    }
+
+    @Test
+    void pull() {
+        String test = "Fabricio";
+
+        for (int i = 0; i < test.length(); i++) {
+            queue.addLast(test.charAt(i));
+        }
+
+        queue.addFirst('H');
+
+        assertEquals(test.length() + 1, queue.size());
+
+        for (int i = 0; i < test.length(); i++) {
+            assertEquals(test.charAt(test.length() - 1 - i), queue.pull());
+        }
+
+        assertEquals('H', queue.pull());
+        assertEquals(0, queue.size());
+
+        assertThrowsExactly(NoSuchElementException.class, () -> queue.pull());
+    }
+
+    @Test
+    void shift() {
+        String test = "Fabricio";
+
+        for (int i = 0; i < test.length(); i++) {
+            queue.addLast(test.charAt(i));
+        }
+
+        queue.addFirst('H');
+
+        assertEquals(test.length() + 1, queue.size());
+        assertEquals('H', queue.shift());
+
+        for (int i = 0; i < test.length(); i++) {
+            assertEquals(test.charAt(i), queue.shift());
+        }
+
+        assertEquals(0, queue.size());
+
+        assertThrowsExactly(NoSuchElementException.class, () -> queue.pull());
+    }
+}
